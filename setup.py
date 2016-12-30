d159dfec97bcdbd6d407998fee9af27406582398
smorrel1 <stephen.morrell@gmail.com>
2016-12-30 18:47:40 +0200

    Packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+# execute with: sudo python setup.py install
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "Greengraph",
+    version = "0.1",
+    packages = find_packages(exclude=['*test']),
+    scripts = ['scripts/greengraph'],
+    install_requires = ['argparse']
+)
