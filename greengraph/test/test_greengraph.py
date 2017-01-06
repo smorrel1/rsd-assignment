a7fd6016042a2a5843aba5438e537b273cec0282
smorrel1 <stephen.morrell@gmail.com>
2017-01-06 22:00:34 +0200

    added test location_sequence

--- greengraph/test/test_greengraph.py
+++ greengraph/test/test_greengraph.py
@@ -5,12 +5,23 @@
 import yaml
 import os
 from greengraph.greengraph import *
-from nose.tools import assert_equal
+from nose.tools import assert_equal  # , assert_list_equal, assert_almost_equal, assert_sequence_equal
+from numpy.testing import assert_array_equal
 def test_func():
+
   # test geolocate
   myGG = Greengraph('London', 'Oxford')
   london_location = myGG.geolocate("London")
   assert_equal(london_location, (51.5073509, -0.1277583))
+
+  # test location_sequence
+  mySequence = myGG.location_sequence([51., 0.], [52., 1.], 3)  # ndarray
+  answer = [[51., 0.], [51.5, 0.5], [52., 1.]]  # tuple
+  assert_array_equal(answer, mySequence, err_msg='location_sequence failed')
+
+  # test green_between
+
+
   # test greengraph_func
   with open(os.path.join(os.path.dirname(__file__),
               'fixtures', 'samples.yaml')) as fixtures_file:
